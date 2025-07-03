59b5cd384c1fc7a886ea46ee064dd4e6434dc89c
donghwan <kdhe9797@naver.com>
2025-07-03 17:37:47 +0900

    .

--- src/main/java/com/nsmm/esg/config_service/ConfigServiceApplication.java
+++ src/main/java/com/nsmm/esg/config_service/ConfigServiceApplication.java
@@ -1,4 +1,4 @@
-package com.nsmm.esg.dart_service.dart_service.config_service;
+package com.nsmm.esg.config_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
